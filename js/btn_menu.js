33ca910ea4a015e51f1f3c5481a57d2c908b2712
David Ohara <davidoharadl@gmail.com>
2018-12-30 22:59:25 -0500

    clicar no item do menu, o menu close

--- js/btn_menu.js
+++ js/btn_menu.js
@@ -1,6 +1,7 @@
 const $menu = document.querySelector('.menu');
 const $btnMenuOpen = document.querySelector('.btnMenu_open');
 const $btnMenuClose = document.querySelector('.btnMenu_close');
+const $itensAction = document.querySelectorAll('.menu-item-action')
 
 $btnMenuOpen.addEventListener('click', function() {
   $menu.classList.add('menu_open');
@@ -8,3 +9,10 @@ $btnMenuOpen.addEventListener('click', function() {
 $btnMenuClose.addEventListener('click', function() {
   $menu.classList.remove('menu_open');
 });
+
+$itensAction.forEach(function($itemAction){
+  $itemAction.addEventListener("click", function(){
+    $menu.classList.remove('menu_open');
+  })
+});
+
